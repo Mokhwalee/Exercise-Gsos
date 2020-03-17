6e4d67ba9feb54259e8f0be0402141f2d70755aa
Mokhwa Lee <54646781+Mokhwalee@users.noreply.github.com>
2020-03-16 22:47:44 -0400

    Medium

---
+++ MontecarloMedium.R
@@ -0,0 +1,22 @@
+### Monte Carlo Integration ###
+### MOKHWA LEE ###
+
+## MEDIUM
+
+#install.packages('cubature')
+library(cubature)
+help(cubature)
+
+# compute the integral of f(x) = exp^{-a||x||^2}
+# region : cube [-1,1]^n
+# various values of a and dimension n until it crashes
+a <- runif(10, -10, 10)
+n <- sample(1:20, 10, replace=F)
+f = function(x){exp(-a*abs(x)^2)}
+
+for (i in 1:10){
+cat("a = ", a[i], "n = ", n[i], "\n")
+print(cubintegrate(f, lower = (-1)^n[i], upper = 1))
+cat("------------------------\n")
+}
+
