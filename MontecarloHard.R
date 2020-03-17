bde1d38e740e275dcc3daf45799122658ed99ec2
Mokhwa Lee <54646781+Mokhwalee@users.noreply.github.com>
2020-03-17 16:08:03 -0400

    Medium

--- MontecarloHard.R
+++ MontecarloHard.R
@@ -3,8 +3,19 @@
 
 ## HARD
 
-# Use volesti to approximate the same integrals as in previous test by simple Monte Carlo
-# uniform sampling and by Importance Sampling using multivariate spherical Gaussian
+# simple Monte Carlo by uniform sampling
+
+f = function(x){exp(-a*abs(x)^2)}
+
+points1 = sample_points(P = NULL, N = NULL, distribution = uniform,
+              WalkType = NULL, walk_step = NULL, exact = NULL, body = NULL,
+              Parameters = NULL, InnerPoint = NULL)
+
+
+# by Importance Sampling using multivariate spherical Gaussian
+points2 = sample_points(P = NULL, N = NULL, distribution = gaussian,
+              WalkType = NULL, walk_step = NULL, exact = NULL, body = NULL,
+              Parameters = NULL, InnerPoint = NULL)
 
 
 # Comment on the accuracy and run-time
