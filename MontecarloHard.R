16888bbc208d06eba08e684b5d11fd5c1f2769b4
Mokhwa Lee <54646781+Mokhwalee@users.noreply.github.com>
2020-03-16 22:48:00 -0400

    Hard

---
+++ MontecarloHard.R
@@ -0,0 +1,10 @@
+### Monte Carlo Integration ###
+### MOKHWA LEE ###
+
+## HARD
+
+# Use volesti to approximate the same integrals as in previous test by simple Monte Carlo
+# uniform sampling and by Importance Sampling using multivariate spherical Gaussian
+
+
+# Comment on the accuracy and run-time
