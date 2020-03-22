f7b49bc4401ae378fcf1041ec6289123f70447b2
Mokhwa Lee <54646781+Mokhwalee@users.noreply.github.com>
2020-03-22 14:57:41 -0400

    Easy

--- MontecarloEasy.R
+++ MontecarloEasy.R
@@ -6,6 +6,7 @@
 # compile and run volesti : install.packages("volesti")
 
 # generate a random H-polytope and compute its volume
+library(Rcpp)
 library(volesti)
 d <- readline(prompt = "Enter The dimension of the convex polytope: \n")
 f <- readline(prompt = "Enter the number of the facets: \n")
@@ -23,8 +24,46 @@ volume(P)
 volume(P,  WalkType = "CDHR", walk_step = 5) 
 volume(P,  WalkType = "RDHR", walk_step = 5)
 volume(P,  WalkType = "BW", walk_step = 5)
-volume(P,  WalkType = "CDHR", walk_step = 100) 
-volume(P,  WalkType = "RDHR", walk_step = 100)
-volume(P,  WalkType = "BW", walk_step = 100)
+volume(P,  WalkType = "CDHR", walk_step = 20) 
+volume(P,  WalkType = "RDHR", walk_step = 20)
+volume(P,  WalkType = "BW", walk_step = 20)
 
 # exact_vol(P) : does not work
+
+# -------------------------------------------------
+## RESULT ##
+
+# > d <- readline(prompt = "Enter The dimension of the convex polytope: \n")
+# Enter The dimension of the convex polytope: 
+#   5
+
+# > f <- readline(prompt = "Enter the number of the facets: \n")
+# Enter the number of the facets: 
+#   15
+
+# > # convert character into integer
+#   > dimension <- as.integer(d)
+# > facet <- as.integer(f)
+
+# > # generate a d-dimensional polytope with f facets
+#   > P = GenRandHpoly(dimension,facet)
+# > P
+# C++ object <0x7fd2bd5659f0> of class 'Hpolytope' <0x7fd2bd572590>
+
+#   > # compute the volume of generated polytope(P)
+#   > volume(P)
+# [1] 17460475
+# > volume(P,  WalkType = "CDHR", walk_step = 5) 
+# [1] 17125822
+# > volume(P,  WalkType = "RDHR", walk_step = 5)
+# [1] 18702053
+# > volume(P,  WalkType = "BW", walk_step = 5)
+# [1] 19735982
+# > volume(P,  WalkType = "CDHR", walk_step = 20) 
+# [1] 18120272
+# > volume(P,  WalkType = "RDHR", walk_step = 20)
+# [1] 18047502
+# > volume(P,  WalkType = "BW", walk_step = 20)
+# [1] 17823575
+# > 
+
