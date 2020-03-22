89d5a2f2b5450cc32609bd8c2ef2ae0d05daddd1
Mokhwa Lee <54646781+Mokhwalee@users.noreply.github.com>
2020-03-22 10:26:35 -0400

    Medium

--- MontecarloMedium.R
+++ MontecarloMedium.R
@@ -9,16 +9,46 @@ help(cubature)
 
 # compute the integral of f(x) = exp^{-a||x||^2}
 # region : cube [-1,1]^n
-# various values of a and dimension n until it crashes
-a <- runif(10, -10, 10)
-n <- sample(1:20, 10, replace=F)
-f = function(x){exp(-a*abs(x)^2)}
 
-for (i in 1:10){
-cat("a = ", a[i], "n = ", n[i], "\n")
-cat("** Hcubature **\n")
-print(hcubature(f, lower = (-1)^n[i], upper = 1))
-cat("** Pcubature **\n")
-print(pcubature(f, lower = (-1)^n[i], upper = 1))
-cat("------------------------\n")
+# various values of a and dimension n untill it crashes
+a <- runif(10, 1, 100)
+n <- sample(10:20, 10, replace=F)
+a
+n
+
+# number of component is one, which is x
+fun = function(x){exp(-a*abs(x)^2)}
+
+for (i in 1:length(a))
+{
+  start.time <- Sys.time()
+  
+  cat("a = ", a[i], "n = ", n[i], "\n")
+  integral = cubintegrate(f=fun, lower=rep(-1,n[i]), upper=rep(1,n[i]), fDim=1)
+  print(integral)
+  
+  if (integral$error > 1)
+  {
+    cat("The result is not accurate\n")
+  }
+  
+  end.time <- Sys.time()
+  time.taken <- end.time - start.time
+  cat("time taken = ", time.taken)
+  
+  if (time.taken > 60)
+  {
+    cat("Running time is more than 1 min\n")
+  }
+  
+  #cat("** Hcubature **\n")
+  #print(hcubature(f, lower = (-1)^n[i], upper = 1))
+  #cat("** Pcubature **\n")
+  #print(pcubature(f, lower = (-1)^n[i], upper = 1))
+  cat("------------------------\n")
 }
+
+# Comments
+# For the bigger number a, f(x) has the bigger constant a in the exponential function and so it could be less accurate.
+# For the bigger number n, error increases because the integration is conducted in higher dimension.
+# Moreover, the running time is more with bigger a and n.
